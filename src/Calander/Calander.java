1afb1009fde95d9f0da125917bf7fb1775fc6390
letgetit <letgetit@218.38.137.27>
2022-12-27 21:59:26 +0900

    calander

--- src/Calander/Calander.java
+++ src/Calander/Calander.java
@@ -2,6 +2,11 @@ package Calander;
 
 public class Calander {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("일  월  화  수  목  금  토 " +"\n");
+		System.out.println("--------------------" +"\n");
+		System.out.println(" 1  2  3  4   5  6   7" +"\n");
+		System.out.println(" 8  9  10 11  12 13  14" +"\n");
+		System.out.println(" 15  16 0 11  12 13  14" +"\n");
+		
 	}
 }
